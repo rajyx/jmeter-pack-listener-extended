864e80c06c28dd851914e127747458c5621bd4d2
egor.matveev <egor.matveev@rtmis.ru>
2023-06-25 18:56:08 +0300

    add test to check samplers filtering

--- src/test/java/ru/loadtest/listeners/clickhouse/samplersbuffer/SamplersBufferTest.java
+++ src/test/java/ru/loadtest/listeners/clickhouse/samplersbuffer/SamplersBufferTest.java
@@ -2,43 +2,50 @@ package ru.loadtest.listeners.clickhouse.samplersbuffer;
 
 import org.apache.jmeter.samplers.SampleResult;
 import org.junit.Test;
-import ru.loadtest.listeners.clickhouse.filter.ISamplersFilter;
 import ru.loadtest.listeners.clickhouse.filter.SamplersFilter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class SamplersBufferTest {
+    private final int DEFAULT_SAMPLRES_QUANTITY = 5;
 
     @Test
-    public void checkAddSamplersUsingSimpleRegex() {
-        String filterRegex = ".*";
-        boolean recordSubSamplers = true;
-        ISamplersFilter samplersFilter = new SamplersFilter()
-                .setFilterRegex(filterRegex);
-        SamplersBuffer samplersBuffer = new SamplersBuffer(
-                samplersFilter,
-                recordSubSamplers
+    public void checkNotMatchedWithRegexSamplersNotExistsInBuffer() {
+        String filterRegex = "\\[HR\\].+";
+        List<SampleResult> inputMatchedSamplers = getSampleResultsInQuantityWithNamePrefix(
+                DEFAULT_SAMPLRES_QUANTITY,
+                "[HR] http request sampler"
         );
-        List<SampleResult> sampleResults = getSampleResults(10);
-        samplersBuffer.addSamplers(sampleResults);
-        assertTrue(
-                "Input Samplers count not equals buffers samplers count",
-                sampleResults.size() == samplersBuffer.getSampleResults().size()
+        List<SampleResult> inputNotMatchedSamplers = getSampleResultsInQuantityWithNamePrefix(
+                DEFAULT_SAMPLRES_QUANTITY,
+                "[TC] transaction controller"
+        );
+        ISamplersBuffer samplersBuffer = new SamplersBuffer(
+                new SamplersFilter()
+                        .setFilterRegex(filterRegex),
+                true
+        );
+        samplersBuffer.addSamplers(inputMatchedSamplers);
+        samplersBuffer.addSamplers(inputNotMatchedSamplers);
+        long matchedSamplersCount = samplersBuffer.getSampleResults()
+                .stream()
+                .filter(sampler -> sampler.getSampleLabel().matches(filterRegex))
+                .count();
+        assertEquals(
+                "Input matched with regex samplers quantity not equals existed samplers in buffer quantity",
+                DEFAULT_SAMPLRES_QUANTITY,
+                matchedSamplersCount
         );
     }
 
-    @Test
-    public void getSampleResults() {
-    }
-
-    private List<SampleResult> getSampleResults(int quantity) {
+    private List<SampleResult> getSampleResultsInQuantityWithNamePrefix(int quantity, String samplerPrefix) {
         List<SampleResult> sampleResults = new ArrayList<>();
         for (int i = 0; i < quantity; i++) {
             SampleResult sampleResult = new SampleResult(System.currentTimeMillis(), 200);
-            sampleResult.setSampleLabel("Samper " + i);
+            sampleResult.setSampleLabel(samplerPrefix + i);
             sampleResult.setSuccessful(true);
             sampleResults.add(sampleResult);
         }
