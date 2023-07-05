2c5360318af5c3826c06a5fd38c6d96be93a14be
egor.matveev <egor.matveev@rtmis.ru>
2023-07-05 08:02:14 +0300

    set up sampler buffer and sampler filter before tests

--- src/main/java/ru/loadtest/listeners/clickhouse/filter/ISamplersFilter.java
+++ src/main/java/ru/loadtest/listeners/clickhouse/filter/ISamplersFilter.java
@@ -2,6 +2,12 @@ package ru.loadtest.listeners.clickhouse.filter;
 
 import org.apache.jmeter.samplers.SampleResult;
 
+import java.util.Set;
+
 public interface ISamplersFilter {
     boolean isSamplerValid(SampleResult sampler);
+
+    SamplersFilter setFilterRegex(String filterRegex);
+
+    SamplersFilter setSamplersToStore(Set<String> samplersToStore);
 }
